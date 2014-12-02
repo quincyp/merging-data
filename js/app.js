f3bb651b0738d96a57e54fd62ff2322186dd16ef
quincyp <quincytpham@gmail.com>
2014-12-01 22:36:52 -0800

    added controller and sort functions in labeled areas

--- js/app.js
+++ js/app.js
@@ -5,3 +5,22 @@
     Add code here to create a new Angular application and controller.
     The array of employee objects is already in a global variable named 'pawneeEmployees'
 */
+angular.module('AddressBook', [])
+    .controller('AddressController', function($scope) {
+        $scope.employees = pawneeEmployees;
+        $scope.sortCol = 'lastName';
+        //controller initialization goes here
+        $scope.sortBy = function(sortCol) {
+            if ($scope.sortCol == sortCol) {
+                $scope.sortReverse = !$scope.sortReverse;
+            } else {
+                $scope.sortCol = sortCol;
+                $scope.sortReverse = false;
+            }
+            $scope.sortCol = sortCol;
+        };
+		
+        $scope.isSortedBy = function(colName) {
+            return colName == $scope.sortCol;
+        }
+    });
